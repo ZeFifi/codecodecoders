f29b911da748b3cbf1e028917580efc1e28058ce
Philippe <philippe@air-de-sylvie.home>
2025-01-20 10:32:36 +0100

    Update footer mentions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export const Footer = () => {
   return (
-    <div className="flex h-20 w-full items-center justify-center border-t border-gray-200 bg-white">
-      <h1 className="text-xs md:text-sm text-gray-600 font-inter">Copyright © {new Date().getFullYear()} - CodeCodeCoders. Tous droits réservés.</h1>
-    </div>
+      <div className="flex flex-col h-20 w-full items-center justify-center border-t border-gray-200 bg-white">
+        <h1 className="text-xs md:text-sm text-gray-600 font-inter">Copyright © {new Date().getFullYear()} - CodeCodeCoders. Tous droits réservés.</h1>
+    
+      <p className="text-xs md:text-sm text-gray-600 font-inter">Hébergé par <Link href="https://www.netlify.com/" target="_blank" className="text-xs md:text-sm text-gray-600 font-inter no-effect underline">Netlify</Link> - 512 2nd Street, Suite 200 San Francisco, CA 94107</p>
+    
+      </div>
   );
 };
